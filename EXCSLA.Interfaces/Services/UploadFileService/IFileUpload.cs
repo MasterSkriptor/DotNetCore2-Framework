7fdff98aef5a42e29fa4d47ec8dc273b3d00613c
masterskriptor <masterskriptor@hotmail.com>
2018-06-29 17:59:24 -0500

    Fixed IFileUpload bug, and repackaged all nugets

--- EXCSLA.Interfaces/Services/UploadFileService/IFileUpload.cs
+++ EXCSLA.Interfaces/Services/UploadFileService/IFileUpload.cs
@@ -7,9 +7,13 @@ namespace EXCSLA.Services.FileUploadServices
     public interface IFileUpload
     {
         Task UploadFilesAsync(ICollection<IFormFile> Files);
+        Task UploadFilesAsync(ICollection<IFormFile> Files, string uploadPath);
         void UploadFiles(ICollection<IFormFile> Files);
+        void UploadFiles(ICollection<IFormFile> Files, string uploadPath);
         Task UploadFileAsync(IFormFile file);
+        Task UploadFileAsync(IFormFile file, string uploadPath);
         void UploadFile(IFormFile file);
+        void UploadFile(IFormFile file, string uploadPath);
         bool FileExists(string fileName);
     }
 }
